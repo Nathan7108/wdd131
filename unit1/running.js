4a2dcf2fff2e714a59e27422fc39e96a268d76ad
NathanLuckock <nathan.luckock@transpara.com>
2025-05-03 21:32:16 -0700

    js

--- unit1/running.js
+++ unit1/running.js
@@ -1,16 +1,19 @@
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
+
 const themeSelector = document.querySelector('#themeSelector');
-const logo = document.querySelector('#logo');
 const body = document.body;
 
 function changeTheme() {
-  const currentTheme = themeSelector.value;
-
-  if (currentTheme === 'dark') {
+  if (themeSelector.value === 'dark') {
     body.classList.add('dark');
-    logo.src = 'byui-logo-white.png';
   } else {
     body.classList.remove('dark');
-    logo.src = 'byui-logo-blue.png';
   }
 }
 
